53ed7146438e5d9b967084531de9c84f7d5fee0c
Barnacle Nightshade <barnaclenshade@protonmail.com>
2021-02-21 18:11:08 +0530

    Create serviceWorker.js

---
+++ dist/js/serviceWorker.js
@@ -0,0 +1,18 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+workbox.precaching.precacheAndRoute([]);
+
+workbox.routing.registerRoute(/\.*$/, new workbox.strategies.NetworkFirst());
+
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+        maxAgeSeconds: 7 * 24 * 60 * 60
+      })
+    ]
+  })
+);
